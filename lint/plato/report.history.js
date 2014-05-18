f3276748dbb37115359d808cee5600becc6bd586
obara.takao <obara.takao@dena.jp>
2014-05-19 05:08:32 +0900

    add getFunctionAttribute, getSearchLink, getReferenceLink

--- lint/plato/report.history.js
+++ lint/plato/report.history.js
@@ -9,5 +9,27 @@ __history = [
       "sloc": 141,
       "maintainability": "64.53"
     }
+  },
+  {
+    "date": "Sun, 18 May 2014 20:01:04 GMT",
+    "total": {
+      "sloc": 449,
+      "maintainability": 66.0108376051388
+    },
+    "average": {
+      "sloc": 449,
+      "maintainability": "66.01"
+    }
+  },
+  {
+    "date": "Sun, 18 May 2014 20:04:15 GMT",
+    "total": {
+      "sloc": 450,
+      "maintainability": 66.1655924200953
+    },
+    "average": {
+      "sloc": 450,
+      "maintainability": "66.17"
+    }
   }
 ]
