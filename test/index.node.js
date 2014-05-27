dd05681e1e70a1279cb7a14cbe42598070c2f99e
uupaa <uupaa.js@gmail.com>
2014-05-28 06:08:17 +0900

    follow new WebModule spec

--- test/index.node.js
+++ test/index.node.js
@@ -1,6 +1,6 @@
 require("../node_modules/uupaa.nodemodule.js/lib/NodeModule.js");
-require("../node_modules/uupaa.console.js/lib/Console.js");
 require("../lib/Reflection.js");
+require("../node_modules/uupaa.console.js/lib/Console.js");
 require("../node_modules/uupaa.help.js/lib/Help.js");
 require("../node_modules/uupaa.valid.js/lib/Valid.js");
 require("../node_modules/uupaa.task.js/lib/Task.js");
